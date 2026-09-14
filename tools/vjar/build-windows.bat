e6cf21a558536778853aad36f3eb336f5cc59a75
alferio <ioan-cristian.alferaru@ec.europa.eu>
2026-09-14 16:12:14 +0200

    ci: fix native bootstrap executable paths

--- tools/vjar/build-windows.bat
+++ tools/vjar/build-windows.bat
@@ -21,14 +21,14 @@ if errorlevel 1 exit /b 1
 clang -include tools\vjar\windows_bootstrap_compat.h -std=c99 -municode -g -w -o v_win_bootstrap.exe vc\v_win.c -llegacy_stdio_definitions -ladvapi32 -lws2_32 -Xlinker /STACK:33554432
 if errorlevel 1 exit /b 1
 
-v_win_bootstrap.exe -new-compiler -no-parallel -gc none -cc clang -cflags "-include tools/vjar/windows_bootstrap_compat.h" -ldflags "-llegacy_stdio_definitions" -o v_stage.exe cmd/v
+.\v_win_bootstrap.exe -new-compiler -no-parallel -gc none -cc clang -cflags "-include tools/vjar/windows_bootstrap_compat.h" -ldflags "-llegacy_stdio_definitions" -o v_stage.exe cmd/v
 if errorlevel 1 exit /b 1
 
-v_stage.exe -new-compiler -prod -cc clang -cflags "-include tools/vjar/windows_bootstrap_compat.h" -ldflags "-llegacy_stdio_definitions" -o v.exe cmd/v
+.\v_stage.exe -new-compiler -prod -cc clang -cflags "-include tools/vjar/windows_bootstrap_compat.h" -ldflags "-llegacy_stdio_definitions" -o v.exe cmd/v
 if errorlevel 1 exit /b 1
 
 if not exist build\native\%TARGET% mkdir build\native\%TARGET%
-v.exe -new-compiler -prod -cc clang -cflags "-include tools/vjar/windows_bootstrap_compat.h" -ldflags "-llegacy_stdio_definitions" -shared -d jar -o build/native/%TARGET%/v cmd/v
+.\v.exe -new-compiler -prod -cc clang -cflags "-include tools/vjar/windows_bootstrap_compat.h" -ldflags "-llegacy_stdio_definitions" -shared -d jar -o build/native/%TARGET%/v cmd/v
 if errorlevel 1 exit /b 1
 
 cl /nologo /LD /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" ^
